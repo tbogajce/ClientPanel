240492f0e8a7297dd2345c1de4cb21cb07f415b6
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-07-11 13:29:01 +0200

    Adding clients table

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -10,12 +10,20 @@ import { Client } from '../../models/Client';
 })
 export class ClientsComponent implements OnInit {
   clients: Client[];
+  totalOwed: number;
 
   constructor(private clientService: ClientService) {}
 
   ngOnInit(): void {
     this.clientService.getClients().subscribe((clients) => {
       this.clients = clients;
+      this.getTotalOwed();
     });
+  }
+
+  getTotalOwed() {
+    this.totalOwed = this.clients.reduce((total, client) => {
+      return total + client.balance;
+    }, 0);
   }
 }
