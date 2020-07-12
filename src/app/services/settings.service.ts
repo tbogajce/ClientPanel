b67266f4ec844ae8cb1c821e85c03f36491e629b
Tihomir Bogajcevic <tihomirbogajcevic94@hotmail.com>
2020-07-12 13:50:19 +0200

    Finishing settings page and adding local storage

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -7,13 +7,21 @@ import { Settings } from '../models/Settings';
 export class SettingsService {
   settings: Settings = {
     allowRegistration: true,
-    disableBalanceOnAdd: false,
-    disableBalanceOnEdit: false,
+    disableBalanceOnAdd: true,
+    disableBalanceOnEdit: true,
   };
 
-  constructor() {}
+  constructor() {
+    if (localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
+  }
 
   getSettings(): Settings {
     return this.settings;
+  }
+
+  changeSettings(settings: Settings) {
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
